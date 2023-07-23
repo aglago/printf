143ede94cacdf44dd7551578f7fe88a969b414af
Ami Manye <amimanyeaglago@gmail.com>
2023-07-23 23:30:31 +0000

    added functions to one file

--- _printf.c
+++ _printf.c
@@ -49,3 +49,36 @@ int _printf(const char *format, ...)
 	va_end(parameter_list);
 	return (count);
 }
+
+
+/**
+ * _putchar - prints character
+ *
+ * desc: character is being printed to standard output
+ *
+ * @c: character to be printed
+ *
+ * Return: the number of characters printed to standard output
+ * i.e 1
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
+
+
+/**
+ * _puts - custom puts
+ * @str: pointer to array of characters (string)
+ * Return: number of characters printed
+ */
+
+int _puts(const char *str)
+{
+	int i = 0;
+
+	for (; str[i] != '\0'; i++)
+		_putchar(str[i]);
+	/* _putchar('\n'); */
+	return (i);
+}
