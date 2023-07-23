dc7fb43487ff58b77d1b886694c1099ffd0fe698
Ami Manye <amimanyeaglago@gmail.com>
2023-07-23 20:36:57 +0000

    adding a header file that contains all function prototypes

---
+++ main.h
@@ -0,0 +1,12 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/* header files */
+#include <stdio.h>
+#include <stdarg.h>
+
+/* function prototypes */
+int _putchar(char c);
+int _printf(const char *format, ...);
+int _puts(const char *str);
+#endif
