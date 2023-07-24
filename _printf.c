962cc10c99a21684e316e8947a8f301ca66e23a3
Ami Manye <amimanyeaglago@gmail.com>
2023-07-24 21:53:42 +0000

    prints (null) if string is null

--- _printf.c
+++ _printf.c
@@ -87,7 +87,10 @@ void specifiers(va_list parameter_list, const char *format, int i, int *count)
 		case 's':
 			par_str = va_arg(parameter_list, const char*);
 			if (par_str == NULL)
+			{
+				(*count) += _puts("(null)");
 				return;
+			}
 			(*count) += _puts(par_str);
 			break;
 		case '%':
