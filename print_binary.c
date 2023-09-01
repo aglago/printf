e2d8ee4b8d9c7caba036f7edcdb5ddbbdab541f8
Ami Manye <amimanyeaglago@gmail.com>
2023-09-01 19:17:22 +0000

    ensure binary representation has 22 characters

--- print_binary.c
+++ print_binary.c
@@ -11,13 +11,23 @@ int print_binary(unsigned int n)
 	int count = 0;
 	int i = 0;
 	int bit = 0;
+	int leading_zeros = 1;
+
 
 	/*binary representation has 22 characters*/
-	for (i = 21; i >= 0; i--)
+	for (i = 31; i >= 0; i--)
 	{
 		bit = (n >> i) & 1;
+
+		if (bit == 0 && leading_zeros)
+			continue;
+
 		count += _putchar('0' + bit);
+		leading_zeros = 0;
 	}
+
+	if (count == 0)
+		count += _putchar('0');
 
 	return (count);
 }
